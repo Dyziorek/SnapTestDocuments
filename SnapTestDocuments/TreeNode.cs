6fb5b2bc42f733ecaed45b91111f9fc232a0ecd6
ddus <ddus@softsystem.pl>
2022-07-18 11:29:30 +0200

    Initial cleanup fo UDX fix

--- SnapTestDocuments/TreeNode.cs
+++ SnapTestDocuments/TreeNode.cs
@@ -3,9 +3,7 @@ using DevExpress.XtraRichEdit.API.Native;
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace SnapTestDocuments
 {
